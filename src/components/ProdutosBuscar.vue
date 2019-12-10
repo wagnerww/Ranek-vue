534990f66824c37e4ba6a825056c0440869c66de
wagner <wagnerricardonet@gmail.com>
2019-12-09 22:46:41 -0300

    impements api fetch

---
+++ src/components/ProdutosBuscar.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="produtos-container"></section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <section>
+    <h1>Compre ou venda</h1>
+    <ProdutosBuscar/>
+    <ProdutosLista/>
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
 };
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
+
