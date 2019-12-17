20364918465d9f0aa4cb5cb810ebd674f01f9d32
wagnerww <wagnerricardonet@gmail.com>
2019-12-17 19:11:31 -0300

    search product

--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -1,11 +1,26 @@
 <template>
-  <p>Produto {{id}}</p>
+  <p>Produto {{produto}}</p>
 </template>
 
 <script>
+import {api} from '@/services';
+
 export default {
   name: "Produtos",
-  props: ["id"]
+  props: ["id"],
+  data(){
+    return {
+      produto: null
+    }
+  },
+  methods:{
+    getProduto(){
+      api.get(`/produto/${this.id}`).then(response => { this.produto = response.data })
+    }
+  },
+  created(){
+    this.getProduto();
+  }
 };
 </script>
 
