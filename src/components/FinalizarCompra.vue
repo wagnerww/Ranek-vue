fea5fc973a7de95b1e79f0a67c40f0318665ac55
wagner <wagnerricardonet@gmail.com>
2020-01-03 21:13:32 -0300

    finalizando transacoes

--- src/components/FinalizarCompra.vue
+++ src/components/FinalizarCompra.vue
@@ -21,7 +21,45 @@ export default {
   computed: {
     ...mapState(["usuario"]),
     compra() {
-      return {};
+      return {
+        comprador_id: this.usuario.email,
+        vendedor_id: this.produto.usuario_id,
+        produto: this.produto,
+        endereco: {
+          cep: this.usuario.cep,
+          rua: this.usuario.rua,
+          numero: this.usuario.numero,
+          bairro: this.usuario.bairro,
+          cidade: this.usuario.cidade,
+          estado: this.usuario.estado
+        }
+      };
+    }
+  },
+  methods: {
+    criarTransacao() {
+      return api.post("/transacao", this.compra).then(() => {
+        this.$router.push({ name: "compras" });
+      });
+    },
+    async criarUsuario() {
+      try {
+        await this.$store.dispatch("criarUsuario", this.$store.state.usuario);
+        await this.$store.dispatch(
+          "getUsuario",
+          this.$store.state.usuario.email
+        );
+        await this.criarTransacao();
+      } catch (error) {
+        //console.log(error);
+      }
+    },
+    finalizarCompra() {
+      if (this.$store.state.login) {
+        this.criarTransacao();
+      } else {
+        this.criarUsuario();
+      }
     }
   }
 };
